4246cd23b2ca2778c8ddf48a9c1dd73cb8c1e24b
Gabriel Ferreira <gabriel.f.viana@ufv.br>
2025-12-12 10:53:49 -0300

    Adiciona classe Estacao com propriedades e ToString()
    Cria a classe Estacao no namespace TopoGente.Core.Entities, incluindo propriedades para Id, Nome, AlturaInstrumento e uma lista de Leituras do tipo LeituraEstacaoTotal. Implementa o método ToString() para exibir informações resumidas da estação.

---
+++ TopoGente.Core/Entities/Estacao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopoGente.Core.Entities
+{
+    public class Estacao
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Nome { get; set; } = string.Empty;
+        public double AlturaInstrumento { get; set; } 
+
+        public List<LeituraEstacaoTotal> Leituras { get; set; } = new List<LeituraEstacaoTotal>();
+
+        public override string ToString()
+        {
+            return $"{Nome} (HI: {AlturaInstrumento} m, Leituras: {Leituras.Count})";
+        }
+    }
+}
